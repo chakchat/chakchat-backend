216dd89629bb92ac6413fce1ff51aa480164de11
sunnyyssh <blit228@mail.ru>
2024-12-27 19:05:03 +0300

    Add different algorithms to JWT

--- identity/internal/jwt/jwt.go
+++ identity/internal/jwt/jwt.go
@@ -1,7 +1,9 @@
 package jwt
 
 import (
+	"crypto/rsa"
 	"errors"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt"
@@ -17,32 +19,54 @@ type Pair struct {
 }
 
 type Config struct {
-	SecureKey []byte
-	Lifetime  time.Duration
-	Issuer    string
-	Audience  string
+	SigningMethod string
+
+	Lifetime time.Duration
+	Issuer   string
+	Audience []string
+
+	publicKey  *rsa.PublicKey  // Used for asymmetric signing
+	privateKey *rsa.PrivateKey // Used for asymmetric signing
+
+	SymmetricKey []byte // Used for symmetric signing
+}
+
+func (c *Config) RSAPublicOnlyKey(key []byte) error {
+	var err error
+	c.publicKey, err = jwt.ParseRSAPublicKeyFromPEM(key)
+	return err
+}
+
+func (c *Config) RSAKeys(privateKey []byte) error {
+	var err error
+	c.privateKey, err = jwt.ParseRSAPrivateKeyFromPEM(privateKey)
+	c.publicKey = &c.privateKey.PublicKey
+	return err
 }
 
 type Claims map[string]any
 
-var nowUTCFunc = func() time.Time {
-	return time.Now().UTC()
+var nowFunc = func() time.Time {
+	return time.Now()
 }
 
 func Generate(config *Config, claims Claims) (Token, error) {
-	if config.Issuer != "" {
-		claims["iss"] = config.Issuer
-	}
-	if config.Audience != "" {
-		claims["aud"] = config.Audience
+	fillBasicClaims(config, claims)
+
+	meth := jwt.GetSigningMethod(config.SigningMethod)
+	if meth == nil {
+		return "", errors.New("no such signing method")
 	}
 
-	now := nowUTCFunc()
-	claims["iat"] = now.Unix()
-	claims["exp"] = now.Add(config.Lifetime).Unix()
+	token := jwt.NewWithClaims(meth, jwt.MapClaims(claims))
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims(claims))
-	signed, err := token.SignedString(config.SecureKey)
+	var signed string
+	var err error
+	if isSymmetricAlg(meth.Alg()) {
+		signed, err = token.SignedString(config.SymmetricKey)
+	} else {
+		signed, err = token.SignedString(config.privateKey)
+	}
 
 	return Token(signed), err
 }
@@ -53,11 +77,53 @@ func Verify(config *Config, token Token) error {
 	return err
 }
 
+// If you need to parse token use only ParseWithAud function because it also verifies.
+func VerifyWithAud(config *Config, token Token, aud string) error {
+	_, err := Parse(config, token)
+	return err
+}
+
 // Parse also verifies token
 func Parse(config *Config, token Token) (Claims, error) {
+	claims, err := parse(config, token)
+	return Claims(claims), err
+}
+
+// In comparison with just Parse it also checks audience
+// Parse also verifies token
+func ParseWithAud(config *Config, token Token, aud string) (Claims, error) {
+	claims, err := parse(config, token)
+	if err != nil {
+		return nil, err
+	}
+
+	if !claims.VerifyAudience(aud, true) {
+		return nil, errors.New("invalid audience")
+	}
+
+	return Claims(claims), nil
+}
+
+func fillBasicClaims(config *Config, claims Claims) {
+	if config.Issuer != "" {
+		claims["iss"] = config.Issuer
+	}
+	if len(config.Audience) != 0 {
+		claims["aud"] = config.Audience
+	}
+
+	now := nowFunc()
+	claims["iat"] = now.Unix()
+	claims["exp"] = now.Add(config.Lifetime).Unix()
+}
+
+func parse(config *Config, token Token) (jwt.MapClaims, error) {
 	claims := jwt.MapClaims{}
 	parsed, err := jwt.ParseWithClaims(string(token), claims, func(t *jwt.Token) (interface{}, error) {
-		return config.SecureKey, nil
+		if isSymmetricAlg(config.SigningMethod) {
+			return config.SymmetricKey, nil
+		}
+		return config.publicKey, nil
 	})
 	if err != nil {
 		return nil, err
@@ -65,8 +131,17 @@ func Parse(config *Config, token Token) (Claims, error) {
 	if !parsed.Valid {
 		return nil, errors.New("token invalid")
 	}
+
+	if config.Issuer != "" && !claims.VerifyIssuer(config.Issuer, true) {
+		return nil, errors.New("invalid issuer")
+	}
+
 	// json unmarshals int to float64 so I don't wanna expose this specificty to the usage
 	claims["iat"] = int64(claims["iat"].(float64))
 	claims["exp"] = int64(claims["exp"].(float64))
-	return Claims(claims), nil
+	return claims, nil
+}
+
+func isSymmetricAlg(alg string) bool {
+	return strings.HasPrefix(alg, "HS")
 }
