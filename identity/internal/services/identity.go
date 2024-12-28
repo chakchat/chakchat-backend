a2d737ee7be66fe5033b935222abd7e03c7301f1
sunnyyssh <blit228@mail.ru>
2024-12-28 17:38:11 +0300

    Implemented internal key issuing

--- identity/internal/services/identity.go
+++ identity/internal/services/identity.go
@@ -1,7 +1,41 @@
 package services
 
-import "errors"
+import (
+	"context"
 
-var (
-	ErrAccessTokenExpired = errors.New("access token expired")
+	"github.com/chakchat/chakchat/backend/identity/internal/jwt"
 )
+
+type IdentityIssuer struct {
+	userConf     *jwt.Config
+	internalConf *jwt.Config
+}
+
+func NewIdentityIssuer(userConf, internalConf *jwt.Config) *IdentityIssuer {
+	return &IdentityIssuer{
+		userConf:     userConf,
+		internalConf: internalConf,
+	}
+}
+
+func (i *IdentityIssuer) Idenitfy(ctx context.Context, token jwt.Token) (jwt.InternalToken, error) {
+	claims, err := jwt.Parse(i.userConf, token)
+	if err != nil {
+		return "", err
+	}
+
+	internalClaims := extractInternal(claims)
+
+	internalToken, err := jwt.Generate(i.internalConf, internalClaims)
+	if err != nil {
+		return "", err
+	}
+	return jwt.InternalToken(internalToken), nil
+}
+
+func extractInternal(claims jwt.Claims) jwt.Claims {
+	return jwt.Claims{
+		"sub":  claims["sub"],
+		"name": claims["name"],
+	}
+}
