d66ff7b6f90f1f965823a267426725e968c06a75
sunnyyssh <blit228@mail.ru>
2025-01-07 13:19:51 +0300

    Add signout request function

---
+++ tests/identity/service-test/signout_test.go
@@ -0,0 +1,28 @@
+package main_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"test/common"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func doSignOutRequest(t *testing.T, refreshToken string) *http.Response {
+	type Request struct {
+		RefreshJWT string `json:"refresh_token"`
+	}
+	reqBody, _ := json.Marshal(Request{
+		RefreshJWT: refreshToken,
+	})
+	req, err := http.NewRequest(http.MethodPut, common.ServiceUrl+"/v1.0/signout", bytes.NewReader(reqBody))
+	require.NoError(t, err)
+	req.Header.Add(common.HeaderIdemotencyKey, uuid.NewString())
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	return resp
+}
