b9b1811f8b63f55dc3513407eadb6d712f764e80
sunnyyssh <blit228@mail.ru>
2025-01-11 16:26:10 +0300

    Add get file service

--- file-storage/internal/services/get_file.go
+++ file-storage/internal/services/get_file.go
@@ -1,5 +1,35 @@
 package services
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 var ErrFileNotFound = errors.New("file not found")
+
+type FileMetaGetter interface {
+	GetFileMeta(uuid.UUID) (*FileMeta, bool, error)
+}
+
+type GetFileService struct {
+	getter FileMetaGetter
+}
+
+func NewGetFileService(getter FileMetaGetter) *GetFileService {
+	return &GetFileService{
+		getter: getter,
+	}
+}
+
+func (s *GetFileService) GetFile(fileId uuid.UUID) (*FileMeta, error) {
+	meta, ok, err := s.getter.GetFileMeta(fileId)
+	if err != nil {
+		return nil, fmt.Errorf("getting file metadata failed: %s", err)
+	}
+	if !ok {
+		return nil, ErrFileNotFound
+	}
+	return meta, nil
+}
