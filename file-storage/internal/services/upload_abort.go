faa101c3a978279786e7801a15afe26e38a12473
sunnyyssh <blit228@mail.ru>
2025-01-12 11:54:20 +0300

    Implemented multipart uplload abort service

---
+++ file-storage/internal/services/upload_abort.go
@@ -0,0 +1,53 @@
+package services
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/google/uuid"
+)
+
+type UploadMetaGetRemover interface {
+	UploadMetaGetter
+	Remove(uuid.UUID) error
+}
+
+type UploadAbortService struct {
+	metaStorage UploadMetaGetRemover
+	client      *s3.Client
+	conf        *S3Config
+}
+
+func NewUploadAbortService(metaStorage UploadMetaGetRemover, client *s3.Client, conf *S3Config) *UploadAbortService {
+	return &UploadAbortService{
+		metaStorage: metaStorage,
+		client:      client,
+		conf:        conf,
+	}
+}
+
+func (s *UploadAbortService) Abort(ctx context.Context, uploadId uuid.UUID) error {
+	meta, ok, err := s.metaStorage.Get(uploadId)
+	if err != nil {
+		return fmt.Errorf("getting upload meta failed: %s", err)
+	}
+	if !ok {
+		return ErrUploadNotFound
+	}
+
+	_, err = s.client.AbortMultipartUpload(ctx, &s3.AbortMultipartUploadInput{
+		Bucket:   aws.String(s.conf.Bucket),
+		Key:      aws.String(meta.Key),
+		UploadId: aws.String(meta.S3UploadId),
+	})
+	if err != nil {
+		return fmt.Errorf("multipart upload aborting failed: %s", err)
+	}
+
+	if err := s.metaStorage.Remove(meta.PublicUploadId); err != nil {
+		return fmt.Errorf("upload meta removing failed: %s", err)
+	}
+	return nil
+}
