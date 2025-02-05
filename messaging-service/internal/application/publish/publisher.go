49369de55c306d71ceecf80508fa366b432752dc
sunnyyssh <blit228@mail.ru>
2025-02-05 15:59:09 +0300

    Add external mq publisher

--- messaging-service/internal/application/publish/publisher.go
+++ messaging-service/internal/application/publish/publisher.go
@@ -1,9 +1,31 @@
 package publish
 
-import "github.com/google/uuid"
+import (
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/application/external"
+	"github.com/google/uuid"
+)
 
 type Event any
 
-type Publisher interface {
-	PublishForUsers([]uuid.UUID, Event) error
+type UserEvent struct {
+	Users []uuid.UUID `json:"users"`
+	Data  Event       `json:"data"`
+}
+
+type Publisher struct {
+	mq external.MqPublisher
+}
+
+func NewPublisher(mq external.MqPublisher) Publisher {
+	return Publisher{
+		mq: mq,
+	}
+}
+
+func (p Publisher) PublishForUsers(users []uuid.UUID, ev Event) error {
+	userEvent := UserEvent{
+		Users: users,
+		Data:  ev,
+	}
+	return p.mq.Publish(userEvent)
 }
