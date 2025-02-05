d7878152e43f7df78940580ff7e77d6a9cdfac9a
sunnyyssh <blit228@mail.ru>
2025-02-05 11:56:10 +0300

    Fix "oops" compilation error

--- messaging-service/internal/domain/group/text_message.go
+++
@@ -1,5 +0,0 @@
-package group
-
-import "github.com/chakchat/chakchat-backend/messaging-service/internal/domain"
-
-func (g *GroupChat) NewTexMessage(sender domain.UserID, text string, replyTo *Message)
