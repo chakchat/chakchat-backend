b80843c53911135fbcb52be83e7ec2ddadba9ed9
sunnyyssh <blit228@mail.ru>
2025-02-05 13:19:55 +0300

    Add secret group chat

---
+++ messaging-service/internal/domain/secgroup/secret_group_chat.go
@@ -0,0 +1,100 @@
+package secgroup
+
+import (
+	"slices"
+
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/domain"
+)
+
+type SecretGroupChat struct {
+	domain.SecretChat
+	Admin   domain.UserID
+	Members []domain.UserID
+
+	Name        string
+	Description string
+	GroupPhoto  domain.URL
+}
+
+func NewSecretGroupChat(admin domain.UserID, members []domain.UserID, name string) (*SecretGroupChat, error) {
+	if err := domain.ValidateGroupInfo(name, ""); err != nil {
+		return nil, err
+	}
+
+	if !slices.Contains(members, admin) {
+		return nil, domain.ErrAdminNotMember
+	}
+
+	normMembers := domain.NormilizeMembers(members)
+
+	return &SecretGroupChat{
+		SecretChat: domain.SecretChat{
+			Chat: domain.Chat{
+				ID: domain.NewChatID(),
+			},
+		},
+		Admin:       admin,
+		Members:     normMembers,
+		Name:        name,
+		Description: "",
+		GroupPhoto:  "",
+	}, nil
+}
+
+func (g *SecretGroupChat) UpdateInfo(name, description string) error {
+	if err := domain.ValidateGroupInfo(name, description); err != nil {
+		return err
+	}
+
+	g.Name = name
+	g.Description = description
+	return nil
+}
+
+func (g *SecretGroupChat) UpdatePhoto(photo domain.URL) error {
+	g.GroupPhoto = photo
+	return nil
+}
+
+func (g *SecretGroupChat) DeletePhoto() error {
+	if g.GroupPhoto == "" {
+		return domain.ErrGroupPhotoEmpty
+	}
+
+	g.GroupPhoto = ""
+	return nil
+}
+
+func (g *SecretGroupChat) AddMember(newMember domain.UserID) error {
+	if g.IsMember(newMember) {
+		return domain.ErrUserAlreadyMember
+	}
+
+	g.Members = append(g.Members, newMember)
+	return nil
+}
+
+func (g *SecretGroupChat) DeleteMember(member domain.UserID) error {
+	if g.Admin == member {
+		return domain.ErrMemberIsAdmin
+	}
+
+	i := slices.Index(g.Members, member)
+	if i == -1 {
+		return domain.ErrUserNotMember
+	}
+
+	g.Members = slices.Delete(g.Members, i, i+1)
+	return nil
+}
+
+func (g *SecretGroupChat) IsMember(user domain.UserID) bool {
+	return slices.Contains(g.Members, user)
+}
+
+func (g *SecretGroupChat) ValidateCanSend(sender domain.UserID) error {
+	if !g.IsMember(sender) {
+		return domain.ErrUserNotMember
+	}
+	return nil
+}
