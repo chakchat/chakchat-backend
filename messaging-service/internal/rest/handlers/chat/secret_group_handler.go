330b553634e9289254394b3f5356c10af6bf7f8d
sunnyyssh <blit228@mail.ru>
2025-02-16 13:30:24 +0300

    Add secret group REST API handler

---
+++ messaging-service/internal/rest/handlers/chat/secret_group_handler.go
@@ -0,0 +1,193 @@
+package chat
+
+import (
+	"context"
+	"time"
+
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/application/dto"
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/application/request"
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/rest/errmap"
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/rest/restapi"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type SecretGroupService interface {
+	CreateGroup(ctx context.Context, req request.CreateSecretGroup) (*dto.SecretGroupChatDTO, error)
+	UpdateGroupInfo(ctx context.Context, req request.UpdateSecretGroupInfo) (*dto.SecretGroupChatDTO, error)
+	DeleteGroup(ctx context.Context, req request.DeleteChat) error
+	AddMember(ctx context.Context, req request.AddMember) (*dto.SecretGroupChatDTO, error)
+	DeleteMember(ctx context.Context, req request.DeleteMember) (*dto.SecretGroupChatDTO, error)
+}
+
+type SecretGroupHandler struct {
+	service SecretGroupService
+}
+
+func NewSecretGroupHandler(service SecretGroupService) *SecretGroupHandler {
+	return &SecretGroupHandler{
+		service: service,
+	}
+}
+
+func (h *SecretGroupHandler) Create(c *gin.Context) {
+	userId := getUserID(c.Request.Context())
+
+	req := struct {
+		Name    string      `json:"name"`
+		Members []uuid.UUID `json:"members"`
+	}{}
+	if err := c.ShouldBindBodyWithJSON(&req); err != nil {
+		restapi.SendUnprocessableJSON(c)
+		return
+	}
+
+	group, err := h.service.CreateGroup(c.Request.Context(), request.CreateSecretGroup{
+		SenderID: userId,
+		Members:  req.Members,
+		Name:     req.Name,
+	})
+	if err != nil {
+		resp := errmap.Map(err)
+		c.JSON(resp.Code, resp.Body)
+		return
+	}
+
+	restapi.SendSuccess(c, newSecretGroupResponse(group))
+}
+
+func (h *SecretGroupHandler) Update(c *gin.Context) {
+	chatId, err := uuid.Parse(c.Param(paramChatID))
+	if err != nil {
+		restapi.SendInvalidChatID(c)
+		return
+	}
+	userId := getUserID(c.Request.Context())
+
+	req := struct {
+		Name        string `json:"name"`
+		Description string `json:"description"`
+	}{}
+	if err := c.ShouldBindBodyWithJSON(&req); err != nil {
+		restapi.SendUnprocessableJSON(c)
+		return
+	}
+
+	group, err := h.service.UpdateGroupInfo(c.Request.Context(), request.UpdateSecretGroupInfo{
+		ChatID:      chatId,
+		SenderID:    userId,
+		Name:        req.Name,
+		Description: req.Description,
+	})
+	if err != nil {
+		resp := errmap.Map(err)
+		c.JSON(resp.Code, resp.Body)
+		return
+	}
+
+	restapi.SendSuccess(c, newSecretGroupResponse(group))
+}
+
+func (h *SecretGroupHandler) AddMember(c *gin.Context) {
+	chatId, err := uuid.Parse(c.Param(paramChatID))
+	if err != nil {
+		restapi.SendInvalidChatID(c)
+		return
+	}
+	userId := getUserID(c.Request.Context())
+
+	memberId, err := uuid.Parse(c.Param(paramMemberID))
+	if err != nil {
+		restapi.SendInvalidMemberID(c)
+		return
+	}
+
+	group, err := h.service.AddMember(c.Request.Context(), request.AddMember{
+		ChatID:   chatId,
+		SenderID: userId,
+		MemberID: memberId,
+	})
+	if err != nil {
+		resp := errmap.Map(err)
+		c.JSON(resp.Code, resp.Body)
+		return
+	}
+
+	restapi.SendSuccess(c, newSecretGroupResponse(group))
+}
+
+func (h *SecretGroupHandler) DeleteMember(c *gin.Context) {
+	chatId, err := uuid.Parse(c.Param(paramChatID))
+	if err != nil {
+		restapi.SendInvalidChatID(c)
+		return
+	}
+	userId := getUserID(c.Request.Context())
+
+	memberId, err := uuid.Parse(c.Param(paramMemberID))
+	if err != nil {
+		restapi.SendInvalidMemberID(c)
+		return
+	}
+
+	group, err := h.service.DeleteMember(c.Request.Context(), request.DeleteMember{
+		ChatID:   chatId,
+		SenderID: userId,
+		MemberID: memberId,
+	})
+	if err != nil {
+		resp := errmap.Map(err)
+		c.JSON(resp.Code, resp.Body)
+		return
+	}
+
+	restapi.SendSuccess(c, newSecretGroupResponse(group))
+}
+
+func (h *SecretGroupHandler) Delete(c *gin.Context) {
+	chatId, err := uuid.Parse(c.Param(paramChatID))
+	if err != nil {
+		restapi.SendInvalidChatID(c)
+		return
+	}
+	userId := getUserID(c.Request.Context())
+
+	err = h.service.DeleteGroup(c.Request.Context(), request.DeleteChat{
+		ChatID:   chatId,
+		SenderID: userId,
+	})
+	if err != nil {
+		resp := errmap.Map(err)
+		c.JSON(resp.Code, resp.Body)
+		return
+	}
+
+	restapi.SendSuccess(c, struct{}{})
+}
+
+type secretGroupResponse struct {
+	ChatID    uuid.UUID `json:"chat_id"`
+	CreatedAt int64     `json:"created_at"`
+
+	AdminID uuid.UUID   `json:"admin_id"`
+	Members []uuid.UUID `json:"members"`
+
+	Name          string `json:"name"`
+	Description   string `json:"description"`
+	GroupPhotoURL string `json:"group_photo_url"`
+
+	Expiration *time.Duration `json:"expiration"`
+}
+
+func newSecretGroupResponse(dto *dto.SecretGroupChatDTO) secretGroupResponse {
+	return secretGroupResponse{
+		ChatID:        dto.ID,
+		CreatedAt:     dto.CreatedAt,
+		AdminID:       dto.Admin,
+		Members:       dto.Members,
+		Name:          dto.Name,
+		Description:   dto.Description,
+		GroupPhotoURL: dto.GroupPhotoURL,
+		Expiration:    dto.Expiration,
+	}
+}
