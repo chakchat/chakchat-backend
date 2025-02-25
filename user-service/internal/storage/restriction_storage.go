e0cebfe5b2997e72ca99c0d05dc00a6281719e88
Ann <aashvareva@edu.hse.ru>
2025-02-26 01:54:42 +0300

    Created db for restrictions

---
+++ user-service/internal/storage/restriction_storage.go
@@ -0,0 +1,45 @@
+package storage
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type UserRestrictions struct {
+	UserId      uuid.UUID        `gorm:"primaryKey"`
+	Phone       FieldRestriction `gorm:"embedded"`
+	DateOfBirth FieldRestriction `gorm:"embedded"`
+}
+
+type FieldRestriction struct {
+	OpenTo         string
+	SpecifiedUsers []uuid.UUID `gorm:"type:text[]"`
+}
+
+type RestrictionStorage struct {
+	db *gorm.DB
+}
+
+func NewRestrictionStorage(db *gorm.DB) *RestrictionStorage {
+	return &RestrictionStorage{
+		db: db,
+	}
+}
+
+func (s *RestrictionStorage) GetRestriction(ctx context.Context, id uuid.UUID) (*UserRestrictions, error) {
+	var restrictions UserRestrictions
+	if err := s.db.WithContext(ctx).First(&restrictions, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrNotFound
+		}
+		return nil, err
+	}
+	return &UserRestrictions{
+		UserId:      restrictions.UserId,
+		Phone:       restrictions.Phone,
+		DateOfBirth: restrictions.DateOfBirth,
+	}, nil
+}
