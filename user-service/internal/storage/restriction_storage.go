09ee89866e331bb858f4d93ca17f992f7fbe1c84
Ann <aashvareva@edu.hse.ru>
2025-03-04 16:43:02 +0300

    Implemented update restriction in storage layer

--- user-service/internal/storage/restriction_storage.go
+++ user-service/internal/storage/restriction_storage.go
@@ -9,6 +9,11 @@ import (
 	"gorm.io/gorm"
 )
 
+type FieldRestriction struct {
+	OpenTo         string
+	SpecifiedUsers []uuid.UUID
+}
+
 type RestrictionStorage struct {
 	db *gorm.DB
 }
@@ -28,4 +33,47 @@ func (s *RestrictionStorage) GetRestriction(ctx context.Context, id uuid.UUID) (
 		return nil, err
 	}
 	return &restrictions, nil
+}
+
+func (s *RestrictionStorage) UpdateRestrictions(ctx context.Context, id uuid.UUID, phone FieldRestriction, date FieldRestriction) (*models.UserRestrictions, error) {
+	var userRestrictions models.UserRestrictions
+	if err := s.db.WithContext(ctx).Where(&models.User{ID: id}).First(&userRestrictions).Error; err != nil {
+		return nil, ErrNotFound
+	}
+
+	var phoneSpecifiedUsers []models.FieldRestrictionUser
+
+	for _, id := range phone.SpecifiedUsers {
+		phoneSpecifiedUsers = append(phoneSpecifiedUsers, models.FieldRestrictionUser{
+			ID:                 uuid.New(),
+			FieldRestrictionId: userRestrictions.Phone.ID,
+			UserID:             id,
+		})
+	}
+
+	var dateSpecifiedUsers []models.FieldRestrictionUser
+
+	for _, id := range date.SpecifiedUsers {
+		dateSpecifiedUsers = append(dateSpecifiedUsers, models.FieldRestrictionUser{
+			ID:                 uuid.New(),
+			FieldRestrictionId: userRestrictions.DateOfBirth.ID,
+			UserID:             id,
+		})
+	}
+	if err := s.db.WithContext(ctx).Save(&models.UserRestrictions{UserId: id,
+		Phone: models.FieldRestriction{
+			OpenTo:         phone.OpenTo,
+			SpecifiedUsers: phoneSpecifiedUsers,
+		},
+		DateOfBirth: models.FieldRestriction{
+			OpenTo:         date.OpenTo,
+			SpecifiedUsers: dateSpecifiedUsers,
+		}}).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrNotFound
+		}
+		return nil, err
+	}
+
+	return &userRestrictions, nil
 }
