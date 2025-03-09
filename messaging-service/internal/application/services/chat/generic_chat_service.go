f1b5bfbf1bd5384002a119e5e6b65de1814cf9c2
sunnyyssh <blit228@mail.ru>
2025-03-09 15:00:24 +0300

    Add generic chat service

--- messaging-service/internal/application/services/chat/generic_chat_service.go
+++ messaging-service/internal/application/services/chat/generic_chat_service.go
@@ -1,3 +1,38 @@
 package chat
 
-type GenericChatService struct{}
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/application/services"
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/application/storage/repository"
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/domain"
+	"github.com/google/uuid"
+)
+
+type GenericChatService struct {
+	repo repository.GenericChatRepository
+}
+
+func NewGenericChatService(repo repository.GenericChatRepository) *GenericChatService {
+	return &GenericChatService{
+		repo: repo,
+	}
+}
+
+func (s *GenericChatService) GetByMemberID(ctx context.Context, memberID uuid.UUID) ([]services.GenericChat, error) {
+	return s.repo.GetByMemberID(ctx, domain.UserID(memberID))
+}
+
+func (s *GenericChatService) GetByChatID(ctx context.Context, memberID uuid.UUID) (*services.GenericChat, error) {
+	chat, err := s.repo.GetByChatID(ctx, domain.ChatID(memberID))
+	if err != nil {
+		if errors.Is(err, repository.ErrNotFound) {
+			return nil, services.ErrChatNotFound
+		}
+		return nil, fmt.Errorf("getting generic chat failed: %s", err)
+	}
+
+	return chat, nil
+}
---
+++ messaging-service/internal/application/storage/repository/generic_chat_repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/application/services"
+	"github.com/chakchat/chakchat-backend/messaging-service/internal/domain"
+)
+
+type GenericChatRepository interface {
+	// Should return empty slice if not found
+	GetByMemberID(context.Context, domain.UserID) ([]services.GenericChat, error)
+	// Should return ErrNotFound if not found
+	GetByChatID(context.Context, domain.ChatID) (*services.GenericChat, error)
+}
