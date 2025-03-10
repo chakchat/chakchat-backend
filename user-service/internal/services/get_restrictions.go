c2e78562d4f46bbc4f39a414646d049e59de02b2
Ann <aashvareva@edu.hse.ru>
2025-03-10 15:59:00 +0300

    Changed get restrction method in service layer according to changes in storage

--- user-service/internal/services/get_restrictions.go
+++ user-service/internal/services/get_restrictions.go
@@ -4,27 +4,26 @@ import (
 	"context"
 	"errors"
 
-	"github.com/chakchat/chakchat-backend/user-service/internal/models"
 	"github.com/chakchat/chakchat-backend/user-service/internal/storage"
 	"github.com/google/uuid"
 )
 
 type GetRestrictionsRepository interface {
-	GetRestrictions(ctx context.Context, id uuid.UUID) (*models.UserRestrictions, error)
+	GetRestrictions(ctx context.Context, id uuid.UUID, field string) (*storage.FieldRestrictions, error)
 }
 
 type GetRestrictionService struct {
-	restrictionRepo GetRestrictionRepository
+	restrictionRepo GetRestrictionsRepository
 }
 
-func NewGetRestrictionService(restrictionRepo GetRestrictionRepository) *GetRestrictionService {
+func NewGetRestrictionService(restrictionRepo GetRestrictionsRepository) *GetRestrictionService {
 	return &GetRestrictionService{
 		restrictionRepo: restrictionRepo,
 	}
 }
 
-func (g *GetRestrictionService) GetRestrictions(ctx context.Context, id uuid.UUID) (*models.UserRestrictions, error) {
-	restr, err := g.restrictionRepo.GetRestriction(ctx, id)
+func (g *GetRestrictionService) GetRestrictions(ctx context.Context, id uuid.UUID, field string) (*storage.FieldRestrictions, error) {
+	restr, err := g.restrictionRepo.GetRestrictions(ctx, id, field)
 	if err != nil {
 		if errors.Is(err, storage.ErrNotFound) {
 			return nil, ErrNotFound
