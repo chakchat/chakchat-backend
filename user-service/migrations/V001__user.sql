7c010c82433f0d67c7f71c9c38a68e486bc65fb1
sunnyyssh <blit228@mail.ru>
2025-03-24 11:54:11 +0300

    Fixed user migrations

--- user-service/migrations/V001__user.sql
+++ user-service/migrations/V001__user.sql
@@ -2,6 +2,8 @@ CREATE SCHEMA IF NOT EXISTS "users";
 
 CREATE TYPE user_field AS ENUM ('date_of_birth', 'phone');
 
+CREATE TYPE field_visibility AS ENUM ('everyone', 'specfied', 'only_me');
+
 CREATE TABLE IF NOT EXISTS users.user (
     id          UUID    PRIMARY KEY,
 	name        TEXT NOT NULL,
@@ -9,16 +11,16 @@ CREATE TABLE IF NOT EXISTS users.user (
 	phone       TEXT,
 	date_of_birth TIMESTAMP WITH TIME ZONE,
 	photo_url    TEXT,
-	created_at   BIGINT NOT NULL
+	created_at   BIGINT NOT NULL,
 
-	date_of_birth_visibility users.user_field DEFAULT 'everyone'
-	phone_visibility       users.user_field  DEFAULT 'everyone'
+	date_of_birth_visibility field_visibility DEFAULT 'everyone',
+	phone_visibility       field_visibility  DEFAULT 'everyone'
 );
 
 CREATE TABLE field_restrictions (
-    owner_user_id UUID REFERENCING users (user_id),
+    owner_user_id UUID REFERENCES users.user (id) ON DELETE CASCADE,
     field_name user_field NOT NULL,
-    permitted_user_id UUID REFERENCING users (user_id),
+    permitted_user_id UUID REFERENCES users.user (id) ON DELETE CASCADE,
     PRIMARY KEY (
         owner_user_id, 
         field_name, 
