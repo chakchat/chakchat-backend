b30659fd575402b5151c2c821f49cfb8f52497d0
sunnyyssh <blit228@mail.ru>
2025-04-02 15:12:33 +0300

    Commit just to save changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
-# Building images via minikube to include it to its registry automatically
-echo "Bulding docker images..."
-minikube image build identity-service/ -t identity:local  
-minikube image build messaging-service/ -t messaging:local  
-minikube image build file-storage-service/ -t file-storage:local
-minikube image build user-service/ -t user:local
+
+debug=false
+for arg in "$@"; do
+    if [ "$arg" == "--debug" ]; then
+            debug=true
+    fi
+done
+
+images=("identity" "file-storage" "messaging" "user")
+paths=("identity-service/" "file-storage-service/" "messaging-service/" "user-service/")
+tag=local
+
+build-parallel() {
+    for ((i in "${#images[@]}"; do
+        minikube image build "${images[$image]}" -t "$image:$tag" &
+    done
+
+    if ! wait; then
+        echo "At least one build failed! Run it in debug mode"
+        exit 1
+    fi
+}
+
+build-debug() {
+    echo "Bulding docker images..."
+    for image in "${!images[@]}"; do
+        if ! minikube image build "${images[$image]}" -t "$image:$tag"; then
+            echo "Building $image failed"
+        fi
+    done
+}
+
+if $debug; then 
+    build-debug
+else
+    build-parallel
+fi
+
+echo "Successfully built docker images!"
