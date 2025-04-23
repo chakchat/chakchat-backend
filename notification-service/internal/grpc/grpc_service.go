951d95108e1d517b5573a20432d16bed40677767
Ann <aashvareva@edu.hse.ru>
2025-04-23 10:24:25 +0300

    Added template for service

---
+++ notification-service/internal/grpc/grpc_service.go
@@ -0,0 +1,17 @@
+package grpc
+
+type GRPCClients struct {
+}
+
+func (c *GRPCClients) GetChatType() (string, error) {
+	return "", nil
+}
+func (c *GRPCClients) GetGroupName() (string, error) {
+	return "", nil
+}
+func (c *GRPCClients) GetUsername() (string, error) {
+	return "", nil
+}
+func (c *GRPCClients) GetChatName() (string, error) {
+	return "", nil
+}
